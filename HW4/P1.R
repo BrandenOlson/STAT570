eeaa59771dbe9a66a3d7b0d57baa4b2c20c042aa
BrandenOlson <branden.olson@colorado.edu>
2017-10-25 07:29:41 -0700

    Use nleqslv for better rootsolving

--- HW4/P1.R
+++ HW4/P1.R
@@ -1,23 +1,25 @@
 library(data.table)
 library(dplyr)
-library(rootSolve) 
+library(invgamma)
+library(nleqslv)
+library(rstan)
 library(xtable)
 
-chiRoots <- function(params) {
+gammaRoots <- function(params) {
     a <- params[1]
     b <- params[2]
-    res <- 0.9 - pchisq(2/a, df=2*b) + pchisq(0.2/a, df=2*b)
-    return(abs(res))
+    f1 <- 0.05 - pgamma(0.1, a, b)
+    f2 <- 0.95 - pgamma(1, a, b)
+    return(c(f1, f2))
 }
 
-roots <- optim(c(20, 1), chiRoots)$par
+roots <- nleqslv(c(3, 2), gammaRoots)$x
 
 a <- roots[1]
 b <- roots[2]
-q1 <- pchisq(2/a, df=2*b) 
-q2 <- pchisq(0.2/a, df=2*b)
 
-cat( "Pr(0.1 <= Gamma(", a, ", ", b, ")) = ", pgamma(1, shape=a, scale=b), '\n', sep='')
+cat("a =", a, ", b =", b, '\n')
+
 
 stress_df <- read.table("stress.csv")
 stress <- stress_df[, -1]
@@ -47,13 +49,41 @@ print(xtable(mean_sd, digits=c(0, 3, 3)),
       include.rownames=FALSE)
 sink()
 
-normalRoots <- function(params) {
-    mu <- params[1]
-    sigma <- params[2]
-    res <- 0.9 - pnorm( (log(30) - mu)/sigma ) -
-            pnorm( (log(0.5) - mu)/sigma )
-    return(res)
+plotHistogram <- function(ys, a, b, length, type) {
+    n <- length(ys)
+    sample_size <- 10000
+    if(type == "gamma") {
+        lambdas <- rgamma(n=sample_size, n + a, b + sum(ys))
+    } else if(type == "invgamma") {
+        lambdas <- rinvgamma(n=sample_size, n + a, 1/(b + sum(ys)))
+    }
+    hist(lambdas, breaks=30, main=paste0("Posterior for length = ", length))
 }
 
-normal_roots <- optim(c(exp(0), exp(1)), normalRoots)$par
+pdf("histograms.pdf", width=10, height=10)
+par(mfrow=c(2, 2))
+for(i in 1:4) {
+    ys <- stress[i, ] 
+    plotHistogram(ys, a, b, lengths[i], "gamma")
+}
+dev.off()
+
+pdf("inv_histograms.pdf", width=10, height=10)
+par(mfrow=c(2, 2))
+for(i in 1:4) {
+    ys <- stress[i, ] 
+    plotHistogram(ys, a, b, lengths[i], "invgamma")
+}
+dev.off()
+
+normalRoots <- function(params, left_prob, right_prob) {
+    mu <- params[1]
+    sigma <- params[2]
+    f1 <- 0.05 - pnorm( (log(left_prob) - mu)/sigma )
+    f2 <- 0.95 - pnorm( (log(right_prob) - mu)/sigma )
+    return(c(f1, f2))
+}
+
+eta_roots <- nleqslv(c(1, 1), normalRoots, left_prob=0.5, right_prob=30)$x
+alpha_roots <- nleqslv(c(1, 1), normalRoots, left_prob=1, right_prob=4)$x
 
