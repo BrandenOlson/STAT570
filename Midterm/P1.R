4cda280e2798ab32c990a59da9c5ced82879871b
BrandenOlson <branden.olson@colorado.edu>
2017-11-08 21:05:56 -0800

    Corrected Problem 1b in tex and code

--- Midterm/P1.R
+++ Midterm/P1.R
@@ -5,17 +5,24 @@ strata_srs <- read.table("MidtermStrataSRS.txt")
 
 ys <- srs$ysrs
 zs <- srs$birthsrs
+n <- length(ys)
 
-mu <- mean(ys)
+mu_y <- mean(ys)
+mu_z <- mean(zs)
 mu_var <- var(ys)/length(ys)
 
 z_true <- 2003.898
-mu_rat <- z_true*mean(ys)/mean(zs)
-mu_rat_var <- z_true^2 * var(ys) / var(zs)
+mu_rat <- z_true*mu_y / mu_z
+mu_rat_var <- z_true^2 * (mu_y/mu_z)^2 * ( var(ys)/(n*mu_y^2) - 
+                                          2*cov(ys, zs)/(n*mu_y*mu_z) +
+                               var(zs)/(n*mu_z^2) )
+ # mu_rat_var <- z_true^2 * sum((ys/zs - mean(ys/zs))^2)/(n - 1)
 
 mod <- lm(ys ~ zs)
 beta1_hat <- mod$coef[2]
 mu_reg <- mean(ys) + beta1_hat*(mean(zs) - z_true)
+Z <- cbind(1, zs)
+mu_reg_var <- (var(ys) + vcov(mod)[2,2] * var(zs))/n
 
 # Problem 2
 
